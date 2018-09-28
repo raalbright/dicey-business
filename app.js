b6b9e9cd03fef8ab7f471ad558937b1ab3e912fa
raalbright <ray.a.albright@gmail.com>
2018-09-28 14:37:12 -0500

    fixed error

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 
-const diceContainer = document.querySelector('#dice-container');
-const dieBtn = document.querySelector('#die-btn');
-const rollBtn = document.querySelector('#roll-die-btn');
-const sumBtn = document.querySelector('#sum-die-btn');
+const diceContainer = document.querySelector( '#dice-container' );
+const dieBtn = document.querySelector( '#die-btn' );
+const rollBtn = document.querySelector( '#roll-die-btn' );
+const sumBtn = document.querySelector( '#sum-die-btn' );
 
 let dice = [];
+
 class Die {
-    constructor() {
+    constructor () {
         this.value;
 
-        this.div = document.createElement('div');
-        this.div.classList.add('die');
-        diceContainer.appendChild(this.div);
-        this.div.addEventListener('click', this.roll.bind(this));
-        this.div.addEventListener('dblclick', (() => {
-            dice = dice.filter((die) => die.value !== this.value);
-            diceContainer.removeChild(this.div);
-        }).bind(this));
+        this.div = document.createElement( 'div' );
+        this.div.classList.add( 'die' );
+        diceContainer.appendChild( this.div );
+        this.div.addEventListener( 'click', this.roll.bind( this ) );
+        this.div.addEventListener( 'dblclick', ( () => {
+            dice = dice.filter( ( die ) => die !== this, this );
+            diceContainer.removeChild( this.div );
+        } ).bind( this ) );
 
         this.roll();
     }
 
-    roll() {
-        this.value = Math.ceil(Math.random() * 6);
+    roll () {
+        this.value = Math.ceil( Math.random() * 6 );
         this.div.innerText = this.value;
     }
 }
 
 
-dieBtn.addEventListener('click', () => {
+dieBtn.addEventListener( 'click', () => {
     const die = new Die();
-    dice.push(die);
-});
+    dice.push( die );
+} );
 
-rollBtn.addEventListener('click', () => {
-    dice.forEach(die => die.roll());
-});
+rollBtn.addEventListener( 'click', () => {
+    console.log( dice );
+    dice.forEach( die => die.roll() );
+} );
 
-sumBtn.addEventListener('click', () => {
-    const sum = dice.reduce((sum, { value }) => sum += value, 0);
-    alert(sum);
-});
+sumBtn.addEventListener( 'click', () => {
+    const sum = dice.reduce( ( sum, { value } ) => sum += value, 0 );
+    alert( sum );
+} );
