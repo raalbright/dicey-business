cfacb21e667d0eb72baff602ef8aec4500f5b997
raalbright <ray.a.albright@gmail.com>
2018-10-01 12:41:18 -0500

    formatting and simplifying

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const diceContainer = document.querySelector( '#dice-container' );
 const dieBtn = document.querySelector( '#die-btn' );
 const rollBtn = document.querySelector( '#roll-die-btn' );
@@ -10,14 +9,16 @@ class Die {
     constructor () {
         this.value;
 
+        this.roll = this.roll.bind(this);
+
         this.div = document.createElement( 'div' );
         this.div.classList.add( 'die' );
         diceContainer.appendChild( this.div );
-        this.div.addEventListener( 'click', this.roll.bind( this ) );
+        this.div.addEventListener( 'click', this.roll );
         this.div.addEventListener( 'dblclick', ( () => {
-            dice = dice.filter( ( die ) => die !== this, this );
+            dice = dice.filter( ( die ) => die !== this );
             diceContainer.removeChild( this.div );
-        } ).bind( this ) );
+        } ) );
 
         this.roll();
     }
@@ -27,19 +28,16 @@ class Die {
         this.value = Math.ceil( Math.random() * 6 );
         this.div.classList.add( `d${ this.value }` );
         this.div.innerText = this.value;
-        // this.div.innerHTML = '<span class="dot"></span>'.repeat(this.value);
+        this.div.innerHTML = '<span class="dot"></span>'.repeat(this.value);
     }
 }
-
 
 dieBtn.addEventListener( 'click', () => {
     const die = new Die();
     dice.push( die );
 } );
 
-rollBtn.addEventListener( 'click', () => {
-    dice.forEach( die => die.roll() );
-} );
+rollBtn.addEventListener( 'click', () => dice.forEach( die => die.roll() ) );
 
 sumBtn.addEventListener( 'click', () => {
     const sum = dice.reduce( ( sum, { value } ) => sum += value, 0 );
